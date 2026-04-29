ad2d8b6aaf4f0fb0e89183ae4e3d3ed4e47575d4
tavorsharf-lang <tavorsharf@gmail.com>
2026-04-29 11:57:25 +0300

    import: src/components/ui/toggle.tsx

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -34,3 +34,10 @@ const Toggle = React.forwardRef<
   <TogglePrimitive.Root
     ref={ref}
     className={cn(toggleVariants({ variant, size, className }))}
+    {...props}
+  />
+))
+
+Toggle.displayName = TogglePrimitive.Root.displayName
+
+export { Toggle, toggleVariants }
