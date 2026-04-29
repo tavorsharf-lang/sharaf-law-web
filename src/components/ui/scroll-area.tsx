f0569c2c700bd519d13df40e616f615cc8fcfedc
tavorsharf-lang <tavorsharf@gmail.com>
2026-04-29 11:54:18 +0300

    import: src/components/ui/scroll-area.tsx

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -34,3 +34,13 @@ const ScrollBar = React.forwardRef<
         "h-full w-2.5 border-l border-l-transparent p-[1px]",
       orientation === "horizontal" &&
         "h-2.5 flex-col border-t border-t-transparent p-[1px]",
+      className
+    )}
+    {...props}
+  >
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+  </ScrollAreaPrimitive.ScrollAreaScrollbar>
+))
+ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
+
+export { ScrollArea, ScrollBar }
